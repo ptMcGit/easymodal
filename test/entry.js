a6aa69c3aa2ccbfb6d4af5ff091c6eec9dc64f06
Zach H <zdhadgraft@gtcc.edu>
2017-01-19 16:03:38 -0500

    Created tests.html to run tests from.

--- test/entry.js
+++ test/entry.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function(){
     switch(document.documentURI.split('/').pop()){
-    case 'index.html':
+    case 'specs-1.html':
         require('./specs/easy-modal_spec.js');
         break;
-    case 'index-2.html':
+    case 'specs-2.html':
         require('./specs/easy-modal_spec-2.js');
         break;
     }
