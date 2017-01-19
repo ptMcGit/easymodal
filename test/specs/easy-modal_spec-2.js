038b105972db4636571fffc02a9111a512418ada
Zach H <zdhadgraft@gtcc.edu>
2017-01-19 15:24:22 -0500

    Fixed bug in stylesheet detection and updated spec.

--- test/specs/easy-modal_spec-2.js
+++ test/specs/easy-modal_spec-2.js
@@ -11,8 +11,15 @@ describe('EasyModal', function(){
     describe('stylesheets', function(){
 
         it('when initialized with true as the third argument, it does not add a stylesheet', function(){
-            expect(document.styleSheets.length)
-                .to.equal(1);
+            expect((function(){
+                var i,
+                    ss = [],
+                    ds = document.styleSheets;
+
+                for(i = 0; i < ds.length; i++)
+                    ss.push(ds[i].id)
+                return ss;})())
+                .to.not.include(em.constructor.name);
         });
 
     });
