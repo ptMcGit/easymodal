245fd84055b1bd0f0cd70dc1dc7f0658317f311b
Zach H <zdhadgraft@gtcc.edu>
2017-01-20 15:11:51 -0500

    mv'ed easy-modal_spec.js to easy-modal_spec-1.js.

--- test/entry.js
+++ test/entry.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function(){
     switch(document.documentURI.split('/').pop()){
     case 'specs-1.html':
-        require('./specs/easy-modal_spec.js');
+        require('./specs/easy-modal_spec-1.js');
         break;
     case 'specs-2.html':
         require('./specs/easy-modal_spec-2.js');
