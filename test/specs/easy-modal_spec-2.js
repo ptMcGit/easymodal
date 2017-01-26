e6308ee429037b43e57bd7a03004ddf420b3c531
Zach H <zdhadgraft@gtcc.edu>
2017-01-26 10:50:04 -0500

    Simplified easy-modal_spec-2.

--- test/specs/easy-modal_spec-2.js
+++ test/specs/easy-modal_spec-2.js
@@ -2,7 +2,7 @@ var expect = chai.expect;
 
 describe('EasyModal', function() {
     var EasyModal = require('../../index.js');
-
+    var ssCount = document.styleSheets.length;
     var arg1 = 'test-content';
     var arg2 = 'test-button';
     new EasyModal('#' + arg1, '#' + arg2, true);
@@ -11,16 +11,7 @@ describe('EasyModal', function() {
         it('when initialized with true as the third argument,' +
            'it does not add a stylesheet',
            function() {
-               expect((function() {
-                   var i;
-                   var ss = [];
-                   var ds = document.styleSheets;
-
-                   for(i = 0; i < ds.length; i++)
-                       ss.push(ds[i].id);
-                   return ss;
-               })())
-                   .to.not.include(em.constructor.name);
+               expect(ssCount).to.equal(document.styleSheets.length);
            });
     });
 });
